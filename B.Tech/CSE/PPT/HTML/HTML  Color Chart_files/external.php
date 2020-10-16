34843f964777baf54f4800af13e44a8a6da2535a
Zenix27 <agni.utk27@gmail.com>
2020-10-16 10:43:14 +0530

    Added HTML/revamped file paths.

---
+++ B.Tech/CSE/PPT/HTML/HTML  Color Chart_files/external.php
@@ -0,0 +1,25 @@
+
+	function thread(threadid, title, poster, threaddate, threadtime)
+	{
+		this.threadid = threadid;
+		this.title = title;
+		this.poster = poster;
+		this.threaddate = threaddate;
+		this.threadtime = threadtime;
+	}
+	var threads = new Array(15);
+	threads[0] = new thread(14308, 'PHP Tutorial Error', 'JJ2K', '07-01-2009', '12:20 PM');
+	threads[1] = new thread(14303, 'Data::Dumper', 'JTOnline', '07-01-2009', '08:08 AM');
+	threads[2] = new thread(14300, 'Rtf To Html', 'sumeshtg1', '07-01-2009', '03:38 AM');
+	threads[3] = new thread(14298, 'Sorry Complete newbee', 'Busby', '07-01-2009', '12:47 AM');
+	threads[4] = new thread(14297, 'css header with image', 'snakesmum', '06-30-2009', '11:06 PM');
+	threads[5] = new thread(14295, 'To delete only the specific row of the data from the result when i submit the form', 'banban', '06-30-2009', '09:54 PM');
+	threads[6] = new thread(14287, '$_SESSION problems', 'Scotty', '06-30-2009', '04:20 PM');
+	threads[7] = new thread(14286, 'Line breaks after variables?', 'samiax', '06-30-2009', '04:15 PM');
+	threads[8] = new thread(14285, 'Moving the mouse in Visual Basic 2008', 'Chris11', '06-30-2009', '12:15 PM');
+	threads[9] = new thread(14283, 'Draw a Diagonal String with GD Lib', 'Tanoro', '06-30-2009', '09:36 AM');
+	threads[10] = new thread(14277, 'New user, Help with how to remove dupes in an array', 'cg dude', '06-29-2009', '09:05 PM');
+	threads[11] = new thread(14273, 'virtualization anyone?', 'caffine4lovers', '06-29-2009', '01:30 PM');
+	threads[12] = new thread(14272, 'Extracting the first paragraph of a content', 'lauthiamkok', '06-29-2009', '08:48 AM');
+	threads[13] = new thread(14269, '[error] PHP Form Example', 'PigFeed', '06-29-2009', '07:32 AM');
+	threads[14] = new thread(14264, 'Difference Between Two Times.', 'danielnicholaspeterson', '06-29-2009', '02:19 AM');
