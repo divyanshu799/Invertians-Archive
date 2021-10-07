0cb5e50873e08f4bf8110598410e6efae77415f2
Lee Wang <developer@Lees-MacBook-Pro.local>
2021-10-07 16:19:04 +0100

    Add two numbers solution

---
+++ DSA/AddTwoNumbers.py
@@ -0,0 +1,34 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Print all elements
+
+        result = ListNode(0)
+        tail = result
+        carry = 0
+
+        while l1 or l2 or carry:
+
+            v1 = (l1.val if l1 else 0)
+            v2 = (l2.val if l2 else 0)
+            carry, res = divmod(v1 + v2 + carry, 10)
+
+            tail.next = ListNode(res)
+            tail = tail.next
+
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+
+        return result.next
